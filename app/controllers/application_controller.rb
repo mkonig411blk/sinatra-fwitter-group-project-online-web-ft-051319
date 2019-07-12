80006322e72efa38d5a120bd42416f898c03125f
Margot Konig <margot.konig@blackrock.com>
2019-07-12 14:27:40 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,14 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    # MK CHANGES START
+    enable :sessions
+		set :session_secret, "password_security"
+		# MK CHANGES END
+  end
+  
+  get '/' do 
+    erb :index
   end
 
 end
